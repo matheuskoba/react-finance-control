9d5536df7973971535f6099e28e15f4336e1d816
matheuskoba <matheuskoba2021@icloud.com>
2022-08-05 11:22:00 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const categories: Category = {
+    food: {title: 'Alimentacao', color: 'red', expense: true},
+    rent: {title: 'Aluguel', color: 'red', expense: true},
+    salary: {title: 'Salario', color: 'green', expense: false}
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 7, 15), category: 'food', title: 'McDonals', value: 32.12 },
+    { date: new Date(2022, 7, 15), category: 'food', title: 'BuguerKing', value: 28.12 },
+    { date: new Date(2022, 7, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2022, 7, 18), category: 'salary', title: 'Salario', value: 4500 },
+];
